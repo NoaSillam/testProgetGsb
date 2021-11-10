c1755cb9b1c417ea12aef3f208b70ac4bba76e09
NoaSillam <noasillam@gmail.com>
2021-11-10 14:52:25 +0100

    creation du projet

---
+++ src/entity/medoc.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author noasillam
+ */
+public class medoc {
+    private int idMedoc;
+    
+}
